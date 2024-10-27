4ea59abfb62c1853d93f0ad33306507bdec408ba
Ying Kun Zhan <yingkun@zhan.co.nl>
2024-10-27 22:57:49 +0100

    Update thumbs and add nets

--- footprints/ks27-choc-v1-mx-soldered-and-hotswap.js
+++ footprints/ks27-choc-v1-mx-soldered-and-hotswap.js
@@ -1,7 +1,7 @@
 module.exports = {
     params: {
         designator: 'S',
-        side: 'F',
+        side: 'B',
         from: undefined,
         to: undefined,
         include_switch_hole: {type: 'boolean', value: true},
@@ -69,6 +69,57 @@ module.exports = {
                              (fp_line (start -7 7) (end 7 7) (layer "Eco2.User") (width 0.12))` 
                              : ''}
                             `
+
+        
+        const get_at_coordinates = () => {
+            const pattern = /\(at (-?[\d\.]*) (-?[\d\.]*) (-?[\d\.]*)\)/;
+            const matches = p.at.match(pattern);
+            if (matches && matches.length == 4) {
+                return [parseFloat(matches[1]), parseFloat(matches[2]), parseFloat(matches[3])];
+            } else {
+                return null;
+            }
+        }
+
+        const adjust_point = (x, y) => {
+            const at_l = get_at_coordinates();
+            if(at_l == null) {
+                throw new Error(
+                `Could not get x and y coordinates from p.at: ${p.at}`
+                );
+            }
+            const at_x = at_l[0];
+            const at_y = at_l[1];
+            const at_angle = at_l[2];
+            const adj_x = at_x + x;
+            const adj_y = at_y + y;
+
+            const radians = (Math.PI / 180) * at_angle,
+                cos = Math.cos(radians),
+                sin = Math.sin(radians),
+                nx = (cos * (adj_x - at_x)) + (sin * (adj_y - at_y)) + at_x,
+                ny = (cos * (adj_y - at_y)) - (sin * (adj_x - at_x)) + at_y;
+
+            const point_str = `${nx.toFixed(2)} ${ny.toFixed(2)}`;
+            return point_str;
+        }
+
+        const traces = `
+                        (segment (start ${ adjust_point(-3.574126, -4.469559) }) (end ${ adjust_point(-3.574126, -2.862398) }) (width 0.2) (layer "F.Cu")
+                        (segment (start ${ adjust_point(-2.088961, -5.954724) }) (end ${ adjust_point(-3.574126, -4.469559) }) (width 0.2) (layer "F.Cu")
+                        (segment (start ${ adjust_point(0.514219, -5.877549) }) (end ${ adjust_point(-2.011786, -5.877549) }) (width 0.2) (layer "F.Cu")
+                        (segment (start ${ adjust_point(-2.011786, -5.877549) }) (end ${ adjust_point(-2.088961, -5.954724) }) (width 0.2) (layer "F.Cu")
+                        (segment (start ${ adjust_point(-3.574126, -2.862398) }) (end ${ adjust_point(-2.062398, -2.862398) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(-2.062398, -2.862398) }) (end ${ adjust_point(-2, -2.8) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(7.309856, 3.189232) }) (end ${ adjust_point(7.309856, 4.281802) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(1.4, -3.4) }) (end ${ adjust_point(3.6, -1.2) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(3.6, -1.2) }) (end ${ adjust_point(6, -1.2) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(7.309856, 0.109856) }) (end ${ adjust_point(7.309856, 3.189232) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(-2, -2.8) }) (end ${ adjust_point(-1.4, -3.4) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(7.309856, 4.281802) }) (end ${ adjust_point(5.351498, 6.24016) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(-1.4, -3.4) }) (end ${ adjust_point(1.4, -3.4) }) (width 0.2) (layer "B.Cu")
+                        (segment (start ${ adjust_point(6, -1.2) }) (end ${ adjust_point(7.309856, 0.109856) }) (width 0.2) (layer "B.Cu")
+                       `
 
         return `
         ${standard}
